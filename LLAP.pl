27229136455a5ae1f2ce9bb165e7ec18e6adce93
ZerooCool <adhmm0928@gmail.com>
2023-11-28 18:35:46 +0200

    refactoring

--- LLAP.pl
+++ LLAP.pl
@@ -1,61 +1,30 @@
 :- include('KB.pl').
 
-food(X,S1):-
-    (S1=s0,food(X));(S1=result(A,S),(
-    (A=reqf,food(X1,S),X is X1+1);
-    (A=b1,build1(F,_,_),food(X1,S),X is X1 -F);
-    (A=b2,build2(F,_,_),food(X1,S),X is X1 -F);
-    (A=reqm,food(X,S));
-    (A=reqe,food(X,S)))).
+resources(F,M,E,S1):-
+    (S1=s0, food(F), materials(M), energy(E));
+    (S1=result(A,S), (
+        (A=b1, build1(F1,M1,E1), resources(F2,M2,E2,S), F is F2-F1, M is M2-M1, E is E2-E1);
+    	(A=b2, build2(F1,M1,E1), resources(F2,M2,E2,S), F is F2-F1, M is M2-M1, E is E2-E1);
+        (A=reqf, resources(F1,M,E,S), F is F1+1);
+    	(A=reqm, resources(F,M1,E,S), M is M1+1);
+        (A=reqe, resources(F,M,E1,S), E is E1+1))).
 
-materials(X,S1):-
-    (S1=s0,materials(X));(S1=result(A,S),(
-    (A=reqm,materials(X1,S),X is X1+1);
-    (A=b1,build1(_,M,_),materials(X1,S),X is X1 -M);
-    (A=b2,build2(_,M,_),materials(X1,S),X is X1 -M);
-    (A=reqf,materials(X,S));
-    (A=reqe,materials(X,S)))).
+checkValidBuild(N,S):-
+   ((N=1, build1(F,M,E)); (N=2, build2(F,M,E))),
+    resources(F1,M1,E1,S),
+    F1>=F, M1>=M, E1>=E.
 
-energy(X,S1):-
-    (S1=s0,energy(X));(S1=result(A,S),(
-    (A=reqe,energy(X1,S),X is X1+1);
-    (A=b1,build1(_,_,E),energy(X1,S),X is X1 -E);
-    (A=b2,build2(_,_,E),energy(X1,S),X is X1 -E);
-    (A=reqm,energy(X,S));
-    (A=reqf,energy(X,S)))).
+built(N, result(A,S)):-
+    built(N,S);
+    ((N=1, A=b1, checkValidBuild(1,S)); (N=2, A=b2, checkValidBuild(2,S))).
 
+check(S):-
+    built(1,S), built(2,S).
 
-checkValidB(X,S):-
-   ((X=1,build1(F,M,E));(X=2,build2(F,M,E))),
-    food(F1,S),materials(M1,S),energy(E1,S),
-    F1>=F,M1>=M,E1>=E.
-
-built(result(A,S), N):-
-    built(S,N);
-    ((N=1,A=b1,checkValidB(1,S));(N=2,A=b2,checkValidB(2,S))).
+ids(X,L):-
+    (call_with_depth_limit(check(X),L,R), number(R));
+    (call_with_depth_limit(check(X),L,R), R=depth_limit_exceeded,
+    L1 is L+1, ids(X,L1)).
 
 goal(S):-
     ids(S,1).
-
-goal2(S):-
-    built(S,1),built(S,2).
-
-ids(X,L):-
-    (call_with_depth_limit(goal2(X),L,R), number(R));
-    (call_with_depth_limit(goal2(X),L,R), R=depth_limit_exceeded,
-    L1 is L+1, ids(X,L1)).
-
-% ids(X,L):-
-%     (call_with_depth_limit(goal(X),L,R), number(R));
-%     (call_with_depth_limit(goal(X),L,R), R=depth_limit_exceeded,
-%     L1 is L+1, ids(X,L1)).
-
- %to display the whole succ state:
- %      ?- set_prolog_flag(answer_write_options,[max_depth(0)]).
-
- %to run:
- %      ?- ids(X,1).   
-%goal(result(b1, result(reqf, result(reqm, result(reqm, result(reqe, result(b2, result(reqf, result(reqf, result(reqf, result(reqm, s0))))))))))).
-%goal(result(b1, result(reqf, result(reqm, result(b2, result(reqf, result(reqm,result(reqm, result(reqf, result(reqe, result(reqf, s0))))))))))).
-%goal(result(b2, result(reqf, result(reqf, result(reqf, result(reqf, result(reqm, result(reqe, result(b1, result(reqm, result(reqm, s0))))))))))).
-%goal(result(b1, result(reqf, s0)))
